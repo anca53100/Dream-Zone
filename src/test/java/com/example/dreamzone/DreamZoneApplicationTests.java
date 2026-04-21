19ea373f9cf499cc788d60c9f01224f4be4f499d
anca <anca53100@gmail.com>
2026-04-21 08:38:17 -0500

    Initial commit

---
+++ src/test/java/com/example/dreamzone/DreamZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dreamzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamZoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
