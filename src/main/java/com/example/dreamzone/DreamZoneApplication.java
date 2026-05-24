5e93b16a20cd3838a36d2f49618412643ce90015
anca <anca53100@gmail.com>
2026-05-24 12:14:04 -0500

    Modulo de admin terminado

--- src/main/java/com/example/dreamzone/DreamZoneApplication.java
+++ src/main/java/com/example/dreamzone/DreamZoneApplication.java
@@ -1,11 +1,7 @@
 package com.example.dreamzone;
 
-import com.example.dreamzone.model.Producto;
-import com.example.dreamzone.repository.ProductoRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DreamZoneApplication {
@@ -14,18 +10,4 @@ public class DreamZoneApplication {
         SpringApplication.run(DreamZoneApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner testMongo(ProductoRepository repo) {
-        return args -> {
-            Producto p = new Producto();
-            p.setNombre("Figura Satoru Gojo");
-            p.setCategoria("Figuras");
-            p.setPrecio(899.00);
-            p.setStock(15);
-            p.setEstado("Activo");
-            p.setEsNuevo(true);
-            repo.save(p);
-            System.out.println("✅ MongoDB conectado — productos: " + repo.count());
-        };
-    }
 }
--- src/main/java/com/example/dreamzone/model/Producto.java
+++ src/main/java/com/example/dreamzone/model/Producto.java
@@ -3,6 +3,8 @@ package com.example.dreamzone.model;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -21,10 +23,11 @@ public class Producto {
     private boolean esNuevo;
     private boolean esOferta;
     private double precioAnterior;
-    private List<String> imagenes; // rutas relativas
     private String serie;          // "Jujutsu Kaisen", "One Piece", etc.
 
     // Detalles extra
-    private String altura;
+    private String talla;
     private String material;
+    private String imagen;
+
 }
